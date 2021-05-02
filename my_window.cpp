8512c6de9e94b65805015c623717b409c0a8d8a0
salim OUARAB <salim.ouarab@outlook.com>
2021-05-02 21:08:41 +0200

    Classe my_window

---
+++ my_window.cpp
@@ -0,0 +1,5 @@
+#include "my_window.h"
+
+MyWindow::MyWindow()
+{
+}
---
+++ my_window.h
@@ -0,0 +1,10 @@
+#ifndef MY_WINDOW_H
+#define MY_WINDOW_H
+
+class MyWindow
+{
+public:
+    MyWindow();
+};
+
+#endif // MY_WINDOW_H
